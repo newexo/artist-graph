52ac3288cc27deceafebfac71be0951f9ca5c8eb
Reuben Brasher <rkbrasher@gmail.com>
2024-10-08 19:31:08 -0700

    Download IMDb academic data files

---
+++ scripts/README.md
@@ -0,0 +1,9 @@
+# Scripts to construct and use artist graphs
+
+## IMDb S3 data
+
+Note that these scripts should be used for academic purposes only according to IMDb's terms of service. See 
+[IMDb_DISCLAMER.md](IMDb_DISCLAMER.md).
+
+### IMDb data download
+
---
+++ scripts/imdb_download.py
@@ -0,0 +1,57 @@
+# Internet Movie Database Inc. makes publicly available a limited dataset for academic purposes.
+# See https://www.imdb.com/interfaces/
+# Carefully check license to make sure this is matches you intended use.
+
+import os
+from tqdm import tqdm
+import requests
+from sqlalchemy import create_engine, Engine
+
+
+from art_graph import directories
+
+
+def retrieve_imdb_data(filename):
+    path = directories.data(filename)
+    print(path)
+    if os.path.exists(path):
+        print("{} already exists".format(path))
+        return
+    url = "https://datasets.imdbws.com/{}".format(filename)
+    print("Down loading {}".format(url))
+    response = requests.get(url, stream=True)
+    total_size_in_bytes = int(response.headers.get("content-length", 0))
+    block_size = 1024
+    progress_bar = tqdm(total=total_size_in_bytes, unit="iB", unit_scale=True)
+    with open(path, "wb") as f:
+        for data in response.iter_content(block_size):
+            progress_bar.update(len(data))
+            f.write(data)
+    progress_bar.close()
+
+
+def db_path() -> str:
+    return directories.data("IM.db")
+
+
+def get_engine() -> Engine:
+    absolute_path = db_path()
+    return create_engine(f'sqlite:////{absolute_path}')
+
+
+def main():
+    print("Downloading data")
+    retrieve_imdb_data("name.basics.tsv.gz")
+    retrieve_imdb_data("title.basics.tsv.gz")
+    retrieve_imdb_data("title.ratings.tsv.gz")
+    retrieve_imdb_data("title.principals.tsv.gz")
+
+    engine = get_engine()
+    print(engine.url)
+
+    os.system(f"s32cinemagoer.py {directories.data()} {engine.url} ")
+
+
+
+if __name__ == "__main__":
+    main()
