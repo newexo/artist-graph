866782f94dcd3233cf27988eb004365c69e22025
Reuben Brasher <rkbrasher@gmail.com>
2024-10-15 15:07:02 -0700

    Insert all blocks using ORM style and session

--- art_graph/tests/imdb_non_commercial/test_table_read_write.py
+++ art_graph/tests/imdb_non_commercial/test_table_read_write.py
@@ -8,172 +8,176 @@ from ...cinema_data_providers.imdb_non_commercial import (
 )
 
 
-@pytest.fixture
-def connection(engine):
-    return engine.connect()
-
-
-def test_read_write_name_basics(
-    table_name_basics, connection, name_basics_data, session
-):
+def test_read_write_name_basics(table_name_basics, name_basics_data, session):
     block = name_basics_data
-    insert = table_name_basics.insert()
-    with connection.begin() as transaction:
-        try:
-            connection.execute(insert, block)
-        except Exception as e:
-            pytest.fail(f"Error inserting data: {e}")
-        finally:
-            results = (
-                session.query(imdb_orm.NameBasics)
-                .order_by(imdb_orm.NameBasics.nconst)
-                .all()
-            )
-            pyd_objs = [
-                imdb_pyd.NameBasics.model_validate(r) for r in results
-            ]  # convert to pydantic objects
-            actual = [
-                dict(pyd) for pyd in pyd_objs
-            ]  # convert to list of dictionaries
-            assert actual == block
-            transaction.rollback()
+    # First validate the data
+    pyd_block = [imdb_pyd.NameBasics(**r) for r in block]
+    # Turn into ORM objects
+    orm_block = [imdb_orm.NameBasics(**dict(r)) for r in pyd_block]
+    try:
+        # Insert the data
+        session.add_all(orm_block)
+
+        # Read data back and verify
+        results = (
+            session.query(imdb_orm.NameBasics)
+            .order_by(imdb_orm.NameBasics.nconst)
+            .all()
+        )
+        pyd_objs = [
+            imdb_pyd.NameBasics.model_validate(r) for r in results
+        ]  # convert to pydantic objects
+        actual = [dict(pyd) for pyd in pyd_objs]  # convert to list of dictionaries
+        assert actual == block
+    except Exception as e:
+        pytest.fail(f"Error inserting data: {e}")
+    finally:
+        session.rollback()
 
 
-def test_read_write_title_basics(
-    table_title_basics, connection, title_basics_data, session
-):
+def test_read_write_title_basics(table_title_basics, title_basics_data, session):
     block = title_basics_data
-    insert = table_title_basics.insert()
-    with connection.begin() as transaction:
-        try:
-            connection.execute(insert, block)
-        except Exception as e:
-            pytest.fail(f"Error inserting data: {e}")
-        finally:
-            results = (
-                session.query(imdb_orm.TitleBasics)
-                .order_by(imdb_orm.TitleBasics.tconst)
-                .all()
-            )
-            pyd_objs = [
-                imdb_pyd.TitleBasics.model_validate(r) for r in results
-            ]  # convert to pydantic objects
-            actual = [
-                dict(pyd_obj) for pyd_obj in pyd_objs
-            ]  # convert to list of dictionaries
-            assert actual == block
-            transaction.rollback()
+    # First validate the data
+    pyd_block = [imdb_pyd.TitleBasics(**r) for r in block]
+    # Turn into ORM objects
+    orm_block = [imdb_orm.TitleBasics(**dict(r)) for r in pyd_block]
+
+    try:
+        session.add_all(orm_block)
+        results = (
+            session.query(imdb_orm.TitleBasics)
+            .order_by(imdb_orm.TitleBasics.tconst)
+            .all()
+        )
+        pyd_objs = [
+            imdb_pyd.TitleBasics.model_validate(r) for r in results
+        ]  # convert to pydantic objects
+        actual = [
+            dict(pyd_obj) for pyd_obj in pyd_objs
+        ]  # convert to list of dictionaries
+        assert actual == block
+    except Exception as e:
+        pytest.fail(f"Error inserting data: {e}")
+    finally:
+        session.rollback()
 
 
-def test_read_write_title_ratings(
-    table_title_ratings, connection, title_ratings_data, session
-):
+def test_read_write_title_ratings(table_title_ratings, title_ratings_data, session):
     block = title_ratings_data
-    insert = table_title_ratings.insert()
-    with connection.begin() as transaction:
-        try:
-            connection.execute(insert, block)
-        except Exception as e:
-            pytest.fail(f"Error inserting data: {e}")
-        finally:
-            results = (
-                session.query(imdb_orm.TitleRatings)
-                .order_by(imdb_orm.TitleRatings.tconst)
-                .all()
-            )
-            pyd_objs = [
-                imdb_pyd.TitleRatings.model_validate(r) for r in results
-            ]  # convert to pydantic objects
-            actual = [
-                dict(pyd_obj) for pyd_obj in pyd_objs
-            ]  # convert to list of dictionaries
-            assert actual == block
-            transaction.rollback()
+    # First validate the data
+    pyd_block = [imdb_pyd.TitleRatings(**r) for r in block]
+    # Turn into ORM objects
+    orm_block = [imdb_orm.TitleRatings(**dict(r)) for r in pyd_block]
+
+    try:
+        session.add_all(orm_block)
+        results = (
+            session.query(imdb_orm.TitleRatings)
+            .order_by(imdb_orm.TitleRatings.tconst)
+            .all()
+        )
+        pyd_objs = [
+            imdb_pyd.TitleRatings.model_validate(r) for r in results
+        ]  # convert to pydantic objects
+        actual = [
+            dict(pyd_obj) for pyd_obj in pyd_objs
+        ]  # convert to list of dictionaries
+        assert actual == block
+    except Exception as e:
+        pytest.fail(f"Error inserting data: {e}")
+    finally:
+        session.rollback()
 
 
 def test_read_write_title_principals(
-    table_title_principals, connection, title_principals_data, session
+    table_title_principals, title_principals_data, session
 ):
     block = title_principals_data
-    insert = table_title_principals.insert()
-    with connection.begin() as transaction:
-        try:
-            connection.execute(insert, block)
-        except Exception as e:
-            pytest.fail(f"Error inserting data: {e}")
-        finally:
-            results = (
-                session.query(imdb_orm.TitlePrincipals)
-                .order_by(imdb_orm.TitlePrincipals.tconst)
-                .all()
-            )
-            pyd_objs = [
-                imdb_pyd.TitlePrincipals.model_validate(r) for r in results
-            ]  # convert to pydantic objects
-            actual = [
-                dict(pyd_obj) for pyd_obj in pyd_objs
-            ]  # convert to list of dictionaries
-            assert actual == block
-            transaction.rollback()
+    # First validate the data
+    pyd_block = [imdb_pyd.TitlePrincipals(**r) for r in block]
+    # Turn into ORM objects
+    orm_block = [imdb_orm.TitlePrincipals(**dict(r)) for r in pyd_block]
+
+    try:
+        session.add_all(orm_block)
+        results = (
+            session.query(imdb_orm.TitlePrincipals)
+            .order_by(imdb_orm.TitlePrincipals.tconst)
+            .all()
+        )
+        pyd_objs = [
+            imdb_pyd.TitlePrincipals.model_validate(r) for r in results
+        ]  # convert to pydantic objects
+        actual = [
+            dict(pyd_obj) for pyd_obj in pyd_objs
+        ]  # convert to list of dictionaries
+        assert actual == block
+    except Exception as e:
+        pytest.fail(f"Error inserting data: {e}")
+    finally:
+        session.rollback()
 
 
-def test_read_write_title_akas(table_title_akas, connection, title_akas_data, session):
+def test_read_write_title_akas(table_title_akas, title_akas_data, session):
     block = title_akas_data
-    insert = table_title_akas.insert()
-    with connection.begin() as transaction:
-        try:
-            connection.execute(insert, block)
-        except Exception as e:
-            pytest.fail(f"Error inserting data: {e}")
-        finally:
-            results = (
-                session.query(imdb_orm.TitleAkas)
-                .order_by(imdb_orm.TitleAkas.titleId)
-                .all()
-            )
-            pyd_objs = [imdb_pyd.TitleAkas.model_validate(r) for r in results]
-            actual = [dict(pyd_obj) for pyd_obj in pyd_objs]
-            assert actual == block
-            transaction.rollback()
+    # First validate the data
+    pyd_block = [imdb_pyd.TitleAkas(**r) for r in block]
+    # Turn into ORM objects
+    orm_block = [imdb_orm.TitleAkas(**dict(r)) for r in pyd_block]
+
+    try:
+        session.add_all(orm_block)
+        results = (
+            session.query(imdb_orm.TitleAkas).order_by(imdb_orm.TitleAkas.titleId).all()
+        )
+        pyd_objs = [imdb_pyd.TitleAkas.model_validate(r) for r in results]
+        actual = [dict(pyd_obj) for pyd_obj in pyd_objs]
+        assert actual == block
+    except Exception as e:
+        pytest.fail(f"Error inserting data: {e}")
+    finally:
+        session.rollback()
 
 
-def test_read_write_title_crew(table_title_crew, connection, title_crew_data, session):
+def test_read_write_title_crew(table_title_crew, title_crew_data, session):
     block = title_crew_data
-    insert = table_title_crew.insert()
-    with connection.begin() as transaction:
-        try:
-            connection.execute(insert, block)
-        except Exception as e:
-            pytest.fail(f"Error inserting data: {e}")
-        finally:
-            results = (
-                session.query(imdb_orm.TitleCrew)
-                .order_by(imdb_orm.TitleCrew.tconst)
-                .all()
-            )
-            pyd_objs = [imdb_pyd.TitleCrew.model_validate(r) for r in results]
-            actual = [dict(pyd_obj) for pyd_obj in pyd_objs]
-            assert actual == block
-            transaction.rollback()
+    # First validate the data
+    pyd_block = [imdb_pyd.TitleCrew(**r) for r in block]
+    # Turn into ORM objects
+    orm_block = [imdb_orm.TitleCrew(**dict(r)) for r in pyd_block]
+
+    try:
+        session.add_all(orm_block)
+        results = (
+            session.query(imdb_orm.TitleCrew).order_by(imdb_orm.TitleCrew.tconst).all()
+        )
+        pyd_objs = [imdb_pyd.TitleCrew.model_validate(r) for r in results]
+        actual = [dict(pyd_obj) for pyd_obj in pyd_objs]
+        assert actual == block
+    except Exception as e:
+        pytest.fail(f"Error inserting data: {e}")
+    finally:
+        session.rollback()
 
 
-def test_read_write_title_episode(
-    table_title_episode, connection, title_episode_data, session
-):
+def test_read_write_title_episode(table_title_episode, title_episode_data, session):
     block = title_episode_data
-    insert = table_title_episode.insert()
-    with connection.begin() as transaction:
-        try:
-            connection.execute(insert, block)
-        except Exception as e:
-            pytest.fail(f"Error inserting data: {e}")
-        finally:
-            results = (
-                session.query(imdb_orm.TitleEpisode)
-                .order_by(imdb_orm.TitleEpisode.tconst)
-                .all()
-            )
-            pyd_objs = [imdb_pyd.TitleEpisode.model_validate(r) for r in results]
-            actual = [dict(pyd_obj) for pyd_obj in pyd_objs]
-            assert actual == block
-            transaction.rollback()
+    # First validate the data
+    pyd_block = [imdb_pyd.TitleEpisode(**r) for r in block]
+    # Turn into ORM objects
+    orm_block = [imdb_orm.TitleEpisode(**dict(r)) for r in pyd_block]
+
+    try:
+        session.add_all(orm_block)
+        results = (
+            session.query(imdb_orm.TitleEpisode)
+            .order_by(imdb_orm.TitleEpisode.tconst)
+            .all()
+        )
+        pyd_objs = [imdb_pyd.TitleEpisode.model_validate(r) for r in results]
+        actual = [dict(pyd_obj) for pyd_obj in pyd_objs]
+        assert actual == block
+    except Exception as e:
+        pytest.fail(f"Error inserting data: {e}")
+    finally:
+        session.rollback()
