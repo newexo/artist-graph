852aec079b3909f83eb60165037ccd289553741c
Reuben Brasher <rkbrasher@gmail.com>
2024-10-16 20:52:08 -0700

    Add foreign keys

--- art_graph/cinema_data_providers/imdb_non_commercial/imdb_non_commercial_orm_models.py
+++ art_graph/cinema_data_providers/imdb_non_commercial/imdb_non_commercial_orm_models.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Column, Integer, String, Text, Float, Boolean
+from sqlalchemy import Column, Integer, String, Text, Float, Boolean, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
@@ -14,20 +15,7 @@ class NameBasics(Base):
     deathYear = Column(Integer)
     birthYear = Column(Integer)
 
-
-class TitleAkas(Base):
-    __tablename__ = "title_akas"
-
-    id = Column(Integer, primary_key=True, autoincrement=True)
-
-    titleId = Column(Integer)
-    ordering = Column(Integer)
-    attributes = Column(String(127))
-    types = Column(String(31))
-    title = Column(Text)
-    isOriginalTitle = Column(Boolean)
-    language = Column(String(5))
-    region = Column(String(5))
+    title_principals = relationship("TitlePrincipals", back_populates="name_basics")
 
 
 class TitleBasics(Base):
@@ -43,22 +31,11 @@ class TitleBasics(Base):
     runtimeMinutes = Column(Integer)
     genres = Column(Text)
 
-
-class TitleCrew(Base):
-    __tablename__ = "title_crew"
-
-    tconst = Column(Integer, primary_key=True)
-    writers = Column(Text)
-    directors = Column(Text)
-
-
-class TitleEpisode(Base):
-    __tablename__ = "title_episode"
-
-    tconst = Column(Integer, primary_key=True)
-    parentTconst = Column(Integer)
-    seasonNumber = Column(Integer)
-    episodeNumber = Column(Integer)
+    title_principals = relationship("TitlePrincipals", back_populates="title_basics")
+    title_akas = relationship("TitleAkas", back_populates="title_basics")
+    title_crew = relationship("TitleCrew", back_populates="title_basics")
+    title_episode = relationship("TitleEpisode", back_populates="title_basics")
+    title_ratings = relationship("TitleRatings", back_populates="title_basics")
 
 
 class TitlePrincipals(Base):
@@ -66,20 +43,63 @@ class TitlePrincipals(Base):
 
     id = Column(Integer, primary_key=True, autoincrement=True)
 
-    tconst = Column(Integer)
-    nconst = Column(Integer)
+    tconst = Column(Integer, ForeignKey("title_basics.tconst"))
+    nconst = Column(Integer, ForeignKey("name_basics.nconst"))
+
     ordering = Column(Integer)
     job = Column(String(1024))
     category = Column(String(64))
     characters = Column(String(1024))
 
+    title_basics = relationship("TitleBasics", back_populates="title_principals")
+    name_basics = relationship("NameBasics", back_populates="title_principals")
+
+
+class TitleAkas(Base):
+    __tablename__ = "title_akas"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    titleId = Column(Integer, ForeignKey("title_basics.tconst"))
+    ordering = Column(Integer)
+    attributes = Column(String(127))
+    types = Column(String(31))
+    title = Column(Text)
+    isOriginalTitle = Column(Boolean)
+    language = Column(String(5))
+    region = Column(String(5))
+
+    title_basics = relationship("TitleBasics", back_populates="title_akas")
+
+
+class TitleCrew(Base):
+    __tablename__ = "title_crew"
+
+    tconst = Column(Integer, ForeignKey("title_basics.tconst"), primary_key=True)
+    writers = Column(Text)
+    directors = Column(Text)
+
+    title_basics = relationship("TitleBasics", back_populates="title_crew")
+
+
+class TitleEpisode(Base):
+    __tablename__ = "title_episode"
+
+    tconst = Column(Integer, ForeignKey("title_basics.tconst"), primary_key=True)
+    parentTconst = Column(Integer)
+    seasonNumber = Column(Integer)
+    episodeNumber = Column(Integer)
+
+    title_basics = relationship("TitleBasics", back_populates="title_episode")
+
 
 class TitleRatings(Base):
     __tablename__ = "title_ratings"
 
-    tconst = Column(Integer, primary_key=True)
+    tconst = Column(Integer, ForeignKey("title_basics.tconst"), primary_key=True)
     averageRating = Column(Float)
     numVotes = Column(Integer)
+
+    title_basics = relationship("TitleBasics", back_populates="title_ratings")
 
 
 NAME2ORM = {
